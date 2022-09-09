cd465d23e057f1177a60caeec1c623320246713b
Abenezer Zeleke <abenezer.zeleke>
2022-09-09 15:01:15 +0300

    logic implemented

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 // create an express app
 const express = require("express")
-const app = express()
+const app = express();
 
-app.get('/', function(req, res){
-  res.json({ user: 'geek' });
+app.use(express.json());
+
+app.post('/add', function(req, res){
+  const first_number = req.body.first_number;
+  const second_number = req.body.second_number;
+  res.json({ result:  first_number + second_number });
 });
+
+app.post('/subtract', function(req, res){
+  const first_number = req.body.first_number;
+  const second_number = req.body.second_number;
+  res.json({ result:  first_number - second_number });
+});
+
+app.post('/multiply', function(req, res){
+  const first_number = req.body.first_number;
+  const second_number = req.body.second_number;
+  res.json({ result:  first_number * second_number });
+});
+
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
