531231f8754255317208b4b10cdbba74d5eecfa5
Abenezer Zeleke <abenezer.zeleke>
2022-09-09 15:15:25 +0300

    calculate route

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/add', function(req, res){
+app.post('/calculate', function(req, res){
   const first_number = req.body.first_number;
   const second_number = req.body.second_number;
-  res.json({ result:  first_number + second_number });
+  const operation = req.body.operation;
+
+  if(operation == 'add') {
+    res.json({ result:  first_number + second_number });
+  }
+  if(operation == 'subtract') {
+    res.json({ result:  first_number + second_number });
+  }
+  if(operation == 'add') {
+    res.json({ result:  first_number + second_number });
+  }
 });
 
-app.post('/subtract', function(req, res){
-  const first_number = req.body.first_number;
-  const second_number = req.body.second_number;
-  res.json({ result:  first_number - second_number });
-});
-
-app.post('/multiply', function(req, res){
-  const first_number = req.body.first_number;
-  const second_number = req.body.second_number;
-  res.json({ result:  first_number * second_number });
-});
 
 
 // start the server listening for requests
