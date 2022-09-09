b882e5c91aa123a2f88498d557c0ee14dee97126
Abenezer Zeleke <abenezer.zeleke>
2022-09-09 15:18:10 +0300

    calculate route

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ app.post('/calculate', function(req, res){
     res.json({ result:  first_number + second_number });
   }
   if(operation == 'subtract') {
-    res.json({ result:  first_number + second_number });
+    res.json({ result:  first_number - second_number });
   }
-  if(operation == 'add') {
-    res.json({ result:  first_number + second_number });
+  if(operation == 'multiply') {
+    res.json({ result:  first_number * second_number });
   }
+  res.end()
 });
 
 
